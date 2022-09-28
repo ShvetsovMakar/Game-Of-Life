c36cb5c7285b727b6a37a24f6fd1ca62768511bf
ShvetsovMakar <109159699+ShvetsovMakar@users.noreply.github.com>
2022-09-28 19:01:57 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ Field field;
 
 int main()
 {
+	char showNextGeneration;
 	field.firstGenerationSetup();
 	field.fieldDisplay();
 
@@ -11,6 +12,14 @@ int main()
 	{
 		field.nextGenerationCalculation();
 		field.fieldDisplay();
+		
+		cout << "Do you want to see next generation? Type + if you do: ";
+		cin >> showNextGeneration;
+		
+		if (showNextGeneration != '+')
+		{
+			break;
+		}	
 	}
 
 	return 0;

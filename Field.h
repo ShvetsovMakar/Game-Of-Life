f0d431022475e1556eef273b11c0495619ebd972
ShvetsovMakar <109159699+ShvetsovMakar@users.noreply.github.com>
2022-09-28 18:43:16 +0300

    Update Field.h

--- Field.h
+++ Field.h
@@ -16,7 +16,16 @@ private:
 		char cellState = '.';
 	};
 
-	vector <vector <Cell>> field;
+	vector <vector <Cell>> field = {vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')}};
 			
 	char newAliveCell;
 	int aliveNeighboursCounter = 0;

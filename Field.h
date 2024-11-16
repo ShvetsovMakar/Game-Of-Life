d6a665a5d4f5f479f46051b5e78607ac6bb4b6a9
ShvetsovMakar <109159699+ShvetsovMakar@users.noreply.github.com>
2024-11-16 14:37:54 +0300

    Field.h from my computer

--- Field.h
+++ Field.h
@@ -17,17 +17,27 @@ private:
 		Cell(char cellState);
 	};
 
-	vector <vector <Cell>> field = {vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
-									vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')}};
-			
+	vector <vector <Cell>> currentGenerationField = {vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 	 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 	 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+													 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')}};
+
+	vector <vector <Cell>> newGenerationField = {vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')},
+												 vector <Cell> {Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.'), Cell('.')} };
 	char newAliveCell;
 	int aliveNeighboursCounter = 0;
 	int aliveCellRow = 0;
@@ -38,4 +48,3 @@ public:
 	void nextGenerationCalculation();
 	void fieldDisplay();
 };
-
